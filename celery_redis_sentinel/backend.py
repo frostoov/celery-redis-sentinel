019ed88bc1f345aee3330180f67cfe38b5f2eb7d
Alexander Ovechkin <ovov@yandex-team.ru>
2017-07-13 16:50:17 +0300

    Added custom exception handling

--- celery_redis_sentinel/backend.py
+++ celery_redis_sentinel/backend.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from celery.backends.redis import RedisBackend
 from kombu.utils import cached_property
+import json
+import pickle
 from redis import Redis
 
 from .redis_sentinel import EnsuredRedisMixin, get_redis_via_sentinel
@@ -60,3 +62,43 @@ class RedisSentinelBackend(RedisBackend):
             redis_class=type(str('Redis'), (EnsuredRedisMixin, Redis), {}),
             **params
         )
+
+    def prepare_exception(self, exc, serializer=None):
+        exc = self._wrap_exc(exc)
+        return super(RedisBackend, self).prepare_exception(exc, serializer)
+
+    def exception_to_python(self, exc):
+        exc = super(RedisBackend, self).exception_to_python(exc)
+        return self._unwrap_exc(exc)
+
+    @staticmethod
+    def _wrap_exc(e):
+        try:
+            return Exception(json.dumps({
+                'type': type(e).__name__,
+                'module': type(e).__module__,
+                'data': e.serialize() if hasattr(e, 'serialize') else pickle.dumps(e),
+            }))
+        except:
+            return e
+
+    @staticmethod
+    def _unwrap_exc(e):
+        try:
+            exc = json.loads(str(e))
+            cls = getattr(RedisBackend._get_module(exc['module']), exc['type'])
+            if hasattr(cls, 'deserialize'):
+                return cls.deserialize(exc['data'])
+            return pickle.loads(exc['data'])
+        except:
+            return e
+
+    @staticmethod
+    def _get_module(module_name):
+        from importlib import import_module
+        from sys import modules
+        from types import ModuleType
+        module = modules.get(module_name)
+        if isinstance(module, ModuleType):
+            return module
+        return import_module(module_name)
